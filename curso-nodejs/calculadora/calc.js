ec4829ded5be4d18885a6d13a1c528aa6ca48fce
Anderson Sousa <anderson21br@gmail.com>
2019-08-07 21:56:47 -0300

    Atualizacao diversa

---
+++ curso-nodejs/calculadora/calc.js
@@ -0,0 +1,10 @@
+var soma = require("./somar") // o ./ significa que esta na mesma pasta, não é necessario colocar a extenção
+var sub = require("./subtrair")
+var mult = require("./multiplicar")
+var div = require("./dividir")
+
+
+console.log(soma(10, 10));
+console.log(sub(10, 10));
+console.log(mult(10, 10));
+console.log(div(10, 10));
